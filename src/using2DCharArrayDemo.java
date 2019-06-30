27b0ad73a96bc16b76e913c59a2d63e7d14526fb
max bushuev <wowmaxwow98@gmail.com>
2019-06-30 13:05:29 +0300

    Массивы со строками разной длины

--- src/using2DCharArrayDemo.java
+++ src/using2DCharArrayDemo.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+
 public class using2DCharArrayDemo {
+    public static void main(String[] args) {
+        int[] size = {3,12,4,7};
+        char[][] symbs = new char[size.length][];
+        char s = 'A';
+        String txt = "";
+        for (int i = 0; i < symbs.length; i++) {
+            symbs[i] = new char[size[i]];
+            for (int j = 0; j < symbs[i].length; j++) {
+                symbs[i][j] = s;
+                s++;
+                txt+= "| " + symbs[i][j] + " ";
+            }
+            txt+= "|\n";
+        }
+        JOptionPane.showMessageDialog(null,
+                txt,
+                "Массив со строками разной длины",
+                JOptionPane.PLAIN_MESSAGE);
+    }
 }
