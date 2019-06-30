b96077a556d68260a24ddc2218af5e46e266157f
max bushuev <wowmaxwow98@gmail.com>
2019-06-30 16:45:37 +0300

    Разобрано три варианта запуска потока и метод .Join

--- src/MainThread.java
+++ src/MainThread.java
@@ -1,10 +1,78 @@
 public class MainThread {
     public static void main(String[] args) {
-        MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
+//        MyThread t1 = new MyThread();
+//        MyThread t2 = new MyThread();
+//
+//        t1.start();
+//        t2.start();
 
+//        Thread t1 = new Thread(new MyRunnableClass());
+//        Thread t2 = new Thread(new MyRunnableClass());
+//
+//        t1.start();
+//        t2.start();
+
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println(1);
+//            }
+//        }).start();
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println(2);
+//            }
+//        }).start();
+//
+//
+//        System.out.println(3);
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
         t1.start();
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
         t2.start();
+
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("END");
+
+
     }
 }
 
@@ -22,3 +90,18 @@ class MyThread extends Thread {
         }
     }
 }
+
+//class MyRunnableClass implements Runnable {
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < 10; i++) {
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println(" " + i);
+//            }
+//        }
+//}
+
